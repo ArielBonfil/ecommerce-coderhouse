56d95493931b2a553dbe406df358995cfcbf5d22
arielbonfil <stoari33@gmail.com>
2021-02-01 22:19:40 -0300

    .

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./CartWidget.css";
-
 import useAppContext from "../../Context/Context";
+
 export const CartWidget = () => {
   const quantity = useAppContext();
   return <div className="cartWidget">{quantity}</div>;
